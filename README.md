4a5e2167e6677709a31b2229f8acebe3a6b4c134
iamvecr <git@vecr.me>
2023-08-19 14:23:16 +0200

    Rewritten how the script start the execution. Modified README with more details

--- README.md
+++ README.md
@@ -1,41 +1,34 @@
-# Book downloader (Kortext)
+# Kortext book downloader
 
-The book is divided into .jpgs every image is a page, the url to each page has the page number in it. 
+A book is made up of multiple `.jpgs` files, each file is a page, the script will _attempt_ to download each image and save it in the `Book` folder.
 
+Each request needs to be authenticated with a bearer token, which is obtained by logging into Kortext from the browser and inspecting a page request header.
 
-e.g. In the following `25` is the page number 
+### How to get a new Bearer token:
 
-`https://read-api.kortext.com/core/api/v1/reader/draw/123456/25?width=8910&height=10692&pageLabel=25`
-
-To make it easier to inject the page number the url is divided in 2 parts:
-
-`urlInitial` = is the first part of the URL, up until the first page argument 
-
-`detalis` = second part with `width` and `height` parameters
-
-Every request needs to be authorized with a Bearer token.
-
-To obtain a new token;
 1- Login into Kortext;
 
 2- Open the book that you want to download;
 
 3- Use the browser to inspect the network activity ([web search](https://duckduckgo.com/?q=inspect+network+activity))
 
-4- Go to the next page in the book
+4- Move to the next page of the book
 
-5- Find the request for the new page and on the details about the request you can find the `Bearer` token to copy into the code. 
+5- Find the request for the new page and on the details about the request under the request header you can find the `Bearer` token to copy into the code.
 
 ### Requirements
 - NodeJS
 - yarn/npm 
 ### How to
-1- Add the missing parameters into `index.js`;
 
-2- `yarn install` or `npm install`;
+1 - `git clone https://github.com/iamvecr/downloader.git`
 
-3- `node index.js`
+2 - `cd downloader`
 
-4- ???
+3 - Add the missing parameters in `index.js`;
 
-5- Profit
+4- `yarn install` or `npm install`;
+
+5- `node index.js`
+
+
--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const axios = require('axios')                                                          //Used to make GET requests
-const fs = require('fs')
-var totalNumberOfPages = 600                                                            //Change here the number of pages
-var urlInitial = 'https://read-api.kortext.com/core/api/v1/reader/draw/XXXXXXX/'        //Change the XXXXXX with the book ID  
-var details = '?width=8910&height=10692&pageLabel='                                     //Width and Height might change, higher number === higher quality
-var authorization = 'Bearer ';
-var page;
+const fs = require('fs')                                                                //Used to save the requested images
+//------------- Modify the following parameters -------------
+var totalNumberOfPages = 681                                                            //Change here the number of pages
+var baseURl = 'https://read-api.kortext.com/core/api/v1/reader/draw/XXXXXX/'            //Change the XXXXXX with the ID of the book  
+// var parameters = '?width=8910&height=10692&pageLabel='                               //Images are too big ~1.5MB or 2GB> for a book!
+var parameters = '?width=1080&height=1296&pageLabel='                                   //Width and Height might change, higher number === higher quality
+// On Firefox make sure to check the show Raw Data flag and copy the bearer code 
+var authToken = 'Bearer '
 
-(async function loop() {
+const loopRes =  loop()
+
+async function loop() {
     for (page = 0; page < totalNumberOfPages; page++) {
-        var completeURL = urlInitial + page + details + page
-        console.log('url page made up: ' + completeURL);
-        var filepath = './'+page+'.jpg'
+        var completeURL = baseURl + page + parameters + page
+        console.log('Complete URL: ' + completeURL);
+        var filepath = './Book/'+page+'.jpg'                                           //Where to save the images 
         await downloadImage(completeURL, filepath)
         console.log('Finished! Page ' + page + ' of ' + totalNumberOfPages);
     }
-})();
+};
 
 async function downloadImage(url, filepath) {
     const response = await axios({
         url,
         method: 'GET',
         responseType: 'stream',
-        headers : { Authorization: authorization}
+        headers : { Authorization: authToken}
     });
 
 
